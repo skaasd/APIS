fd24e49d578e39eb16a55e834df5d72bde41797b
Jose Rivera <jluis.rivera015@gmail.com>
2025-10-02 21:47:33 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const variable = "Hello, World!";
+console.log(variable);
+alert(variable);
+document.body.style.backgroundColor = "#f0f0f0";
+document.body.style.fontFamily = "Arial, sans-serif";
+document.body.style.color = "#333";
+document.body.style.padding = "20px";
